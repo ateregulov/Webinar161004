20276446e5117d0852791dab13391fe4a63424d3
Teregulov A.R <info@teregulov.com>
2016-10-04 20:26:21 +0300

    возраст пользователя

--- WebApplication1/src/WebApplication1/Models/ApplicationUser.cs
+++ WebApplication1/src/WebApplication1/Models/ApplicationUser.cs
@@ -9,5 +9,7 @@ namespace WebApplication1.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public int Age { get; set; }
+
     }
 }
